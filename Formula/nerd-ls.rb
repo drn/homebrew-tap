bf2dc8b09eac2ac1bdf67ae7ff47391b803a0d5f
Darren Cheng <darren@thanx.com>
2020-03-28 23:44:37 -0700

    Update nerd-ls to v0.1.2.

--- Formula/nerd-ls.rb
+++ Formula/nerd-ls.rb
@@ -1,9 +1,9 @@
 class NerdLs < Formula
   desc "Colorized LS with Nerd Font support"
   homepage "https://github.com/drn/nerd-ls"
-  url "https://github.com/drn/nerd-ls/releases/download/v0.1.1/nerd-ls_0.1.1_Darwin_x86_64.tar.gz"
-  sha256 "ae7200e1f9bd8cc2b9b64536c3cfd7cd390bdf50474a13440db07ca15c1a1bfb"
-  version "0.1.1"
+  url "https://github.com/drn/nerd-ls/releases/download/v0.1.2/nerd-ls_0.1.2_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "2e54e372ed1fbe6874f7256d4e5986f7073ea36e66f36c5d5a788aa7c5ba3e6d"
 
   bottle :unneeded
 
