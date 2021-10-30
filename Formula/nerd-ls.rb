3c8392e9d945d560daadeac09b7e49fc48971088
Darren Cheng <darren@thanx.com>
2021-10-29 22:05:01 -0400

    Suppress bottle :unneeded deprecation warning.

--- Formula/nerd-ls.rb
+++ Formula/nerd-ls.rb
@@ -7,8 +7,6 @@ class NerdLs < Formula
   version VERSION
   sha256 '2e54e372ed1fbe6874f7256d4e5986f7073ea36e66f36c5d5a788aa7c5ba3e6d'
 
-  bottle :unneeded
-
   def install
     bin.install 'nerd-ls'
   end
